5c3f93003b9f3481aca478c1069637df0ed0069d
Federico Ramayo <52141168+xFEF0@users.noreply.github.com>
2024-11-28 17:37:36 -0300

    add Lox statements capability

--- src/main/java/com/fefox/lox/Interpreter.java
+++ src/main/java/com/fefox/lox/Interpreter.java
@@ -1,15 +1,10 @@
 package com.fefox.lox;
 
-public class Interpreter implements Expr.Visitor<Object> {
+import java.util.List;
 
-    void interpret(Expr expression) {
-        try {
-            Object value = evaluate(expression);
-            System.out.println(stringify(value));
-        } catch (RuntimeError error) {
-            Lox.runtimeError(error);
-        }
-    }
+public class Interpreter implements Expr.Visitor<Object>,
+                                    Stmt.Visitor<Void> {
+
 
     @Override
     public Object visitLiteralExpr(Expr.Literal expr) {
@@ -91,6 +86,33 @@ public class Interpreter implements Expr.Visitor<Object> {
         }
 
         return null;
+    }
+
+    @Override
+    public Void visitExpressionStmt(Stmt.Expression stmt) {
+        evaluate(stmt.expression);
+        return null;
+    }
+
+    @Override
+    public Void visitPrintStmt(Stmt.Print stmt) {
+        Object value = evaluate(stmt.expression);
+        System.out.println(stringify(value));
+        return null;
+    }
+
+    void interpret(List<Stmt> statements) {
+        try {
+            for (Stmt statement : statements) {
+                execute(statement);
+            }
+        } catch (RuntimeError error) {
+            Lox.runtimeError(error);
+        }
+    }
+
+    private void execute(Stmt statement) {
+        statement.accept(this);
     }
 
     private Object evaluate(Expr expr) {
